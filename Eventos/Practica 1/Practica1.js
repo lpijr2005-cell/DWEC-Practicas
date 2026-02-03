eeca1c84c707641690b10ce19fbc99149a9ec088
lpijr2005-cell <lpijr2005@gmail.com>
2026-02-03 19:08:24 +0100

    Funciones acabadas

--- Eventos/Practica 1/Practica1.js
+++ Eventos/Practica 1/Practica1.js
@@ -1,7 +1,38 @@
 select = document.getElementById('options');
 
 select.addEventListener('change', function () {
-  if (select.value === 1) {
-    document.createElement('p').textContent('Buenas usuario, ¿como estás?');
+  //Saludar
+  if (select.value === '1') {
+    var mensaje = document.createElement('p');
+    mensaje.textContent = 'Buenos dias tenga usted';
+    document.getElementById('salida').appendChild(mensaje);
+  }
+  //Crear caja roja
+  else if (select.value === '2') {
+    var caja = document.createElement('div');
+
+    caja.textContent = 'Nueva caja';
+    caja.style.backgroundColor = 'red';
+    caja.style.width = '100px';
+    document.getElementById('salida').appendChild(caja);
+  }
+  //Generar número aleatorio entre 1 y 100
+  else {
+    const randomNumber = Math.floor(Math.random() * 100) + 1;
+    document.getElementById('salida').innerHTML +=
+      `<p>El número generado es: ${randomNumber}</p>`;
   }
 });
+
+//Borrar el contenido impreso anteriormente
+function borrar() {
+  var salida = document.getElementById('salida');
+
+  salida.innerHTML = '';
+}
+
+const boton = document.createElement('button');
+boton.addEventListener('click', borrar);
+boton.textContent = 'Reiniciar';
+//Adjuntamos el botón a nuestra página
+document.getElementById('inputs').appendChild(boton);
